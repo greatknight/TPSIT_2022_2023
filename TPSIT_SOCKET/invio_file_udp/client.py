6833f1fec6d5ff84c390c1120c4f12595e55f3bc
Luca Ambrogio <luca.ambrogio04@gmail.com>
2022-11-05 12:42:45 +0100

    invio file con udp

--- TPSIT_SOCKET/invio_file_udp/client.py
+++ TPSIT_SOCKET/invio_file_udp/client.py
@@ -1,31 +1,24 @@
-from asyncio.windows_events import NULL
 from socket import AF_INET, SOCK_DGRAM, socket
 
 BUFFER_SIZE = 1024
 
-#IP = "192.168.136.255"
-#PORT = 5000
-# possibilità
-# localhost 127.0.0.1
-
 def chatClient(ip, port):
-    running = True
     with socket(AF_INET, SOCK_DGRAM) as s:
-        f = open("Pdf_prova.pdf", "rb")
+        f = open("pdf.pdf", "rb")
         buffer = f.read()
-        n = 0
+        #print(buffer)
+        i = 0
         try:
-            while buffer[n * 4096 + 4095]:
-                m = buffer[n * 4096: n * 4096 + 4095]
-                s.sendto(m, (ip, port))
-                n += 1
+            while buffer[i * 4096 + 4095]:
+                message = buffer[i * 4096: i * 4096 + 4095]
+                s.sendto(message, (ip, port))
+                i += 1
         except:
-            m = buffer[n * 4096:]
-        s.sendto(m, (ip, port))
-      
+            message = buffer[i * 4096:]
+        s.sendto(message, (ip, port))
+
 
 if __name__ == "__main__":
     ip = input("Inserisci ip: ")
     port = int(input("Inserisci porta: "))
-    #nome = input("Inserisci nome utente: ")
     chatClient(ip,port)
--- TPSIT_SOCKET/invio_file_udp/server.py
+++ TPSIT_SOCKET/invio_file_udp/server.py
@@ -1,34 +1,28 @@
-from socket import AF_INET, SO_BROADCAST, SOCK_DGRAM, socket
-from ssl import SOL_SOCKET
+from socket import AF_INET, SOCK_DGRAM, socket
 
-#BUFFER_SIZE = 1024
 BUFFER_SIZE = 131072
 
-mystr = "ciao" # str
-# bytes
+mystr = "ciao"
 
 HOST = "0.0.0.0"
 PORT = 5000
-# possibilità
-# localhost 127.0.0.1
 
-def chatServer(HOST, PORT):
-    running = True
+def chatServer():
+    go_on = True
     with socket (AF_INET, SOCK_DGRAM) as s:
         s.bind((HOST, PORT))
-        # s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) LINUX E MACOS
-        img = b""
-        print('In ascolto')
-        while running == True:
+        txt = b""
+        print("Server in ascolto")
+        while go_on == True:
             msg = s.recvfrom(BUFFER_SIZE)
             if len(msg[0]) == 4096:
-                img = img + msg[0]
+                txt = txt + msg[0]
             else:
-                running = False
-        img = img + msg[0]
-        f = open("stampa.pdf", "wb")
-        f.write(img)
-        #print(msg)
+                go_on = False
+        txt = txt + msg[0]
+        f = open("pdf_result.pdf", "wb")
+        f.write(txt)
+        print("done")
 
 if __name__ == "__main__":
     chatServer()
